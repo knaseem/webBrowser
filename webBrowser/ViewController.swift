3a4bdd334d9690611bb945d5eabb90d1968e1901
naseem <naseem@knaseem-mbp.attlocal.net>
2016-08-15 16:18:42 -0400

    Added WebView & allow arbitrary loads for http sites

--- webBrowser/ViewController.swift
+++ webBrowser/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var webView: WKWebView?
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        self.webView = WKWebView(frame: CGRectZero)
+        super.init(coder: aDecoder)!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.addSubview(webView!)
+        webView!.translatesAutoresizingMaskIntoConstraints = false
+        let height = NSLayoutConstraint(item: webView!, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: -44)
+        let width = NSLayoutConstraint(item: webView!, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
+        view.addConstraints([height, width])
+        
+        let url = NSURL(string:"http://www.yahoo.com")
+        let request = NSURLRequest(URL:url!)
+        webView!.loadRequest(request)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+   
+} // End of ViewController Class.
 
