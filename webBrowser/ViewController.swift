47bbc9381232e29f9de7c4c52dc0256f25fc945a
naseem <naseem@knaseem-mbp.attlocal.net>
2016-08-15 16:04:36 -0400

    Initial Commit

---
+++ webBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webBrowser
+//
+//  Created by naseem on 15/08/2016.
+//  Copyright © 2016 naseem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
